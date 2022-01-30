a139eb4f735f501d4abeb89d873b9d31f819ccbd
mario688 <gliwamario@gmail.com>
2022-01-30 19:14:22 +0100

    fixed encoding function

--- src/App.js
+++ src/App.js
@@ -1,55 +1,57 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
-import GoogleLogin from 'react-google-login';
-import Button from '@atlaskit/button';
-
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import GoogleLogin from "react-google-login";
+import Button from "@atlaskit/button";
 
 const responseGoogle = (response) => {
   console.log(response);
-}
+};
 
 export default function App() {
-
   const [accessToken, setAccessToken] = useState();
   const [imageBase64, setImageBase64] = useState();
 
   const setAccessTokenState = (response) => {
     setAccessToken(response.tokenObj.access_token);
-  }
+  };
 
-  const myImage = (
-    {
-      "instances": [{
-        "content": imageBase64,
-      }],
-      "parameters": {
-        "confidenceThreshold": 0.5,
-        "maxPredictions": 5
-      }
-    }
-  )
+  const myImage = {
+    instances: [
+      {
+        content: imageBase64,
+      },
+    ],
+    parameters: {
+      confidenceThreshold: 0.5,
+      maxPredictions: 5,
+    },
+  };
 
-  async function postData(url = 'https://us-central1-aiplatform.googleapis.com/v1/projects/930816262472/locations/us-central1/endpoints/4466101882816823296:predict', data = myImage) {
+  async function postData(
+    url = "https://us-central1-aiplatform.googleapis.com/v1/projects/930816262472/locations/us-central1/endpoints/4466101882816823296:predict",
+    data = myImage
+  ) {
     console.log("wysyłam");
     const response = await fetch(url, {
-      method: 'POST', 
+      method: "POST",
       headers: {
-        'Authorization': 'Bearer '+ accessToken,
-        'Content-Type': 'application/json'
+        Authorization: "Bearer " + accessToken,
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(data)
+      body: JSON.stringify(data),
     });
     console.log(response.json());
-    // return response.json(); 
+    // return response.json();
   }
 
   const sendPhotoForm = (
     <>
-      <input type="file" accept=".png" onChange={e => encodeImageFileAsURL(e.target)} />
-      <Button 
-      appearance="primary"
-      onClick={() => postData()}
-      isDisabled={!imageBase64}>
+      <input type="file" onChange={(e) => encodeImageFileAsURL(e.target)} />
+      <Button
+        appearance="primary"
+        onClick={() => postData()}
+        isDisabled={!imageBase64}
+      >
         Sprawdz zdjęcie
       </Button>
     </>
@@ -58,25 +60,22 @@ export default function App() {
   function encodeImageFileAsURL(element) {
     var file = element.files[0];
     var reader = new FileReader();
-    reader.onloadend = function() {
-      setImageBase64(reader.result.slice(22))
-    }
+    reader.onloadend = function () {
+      setImageBase64(reader.result.replace("data:", "").replace(/^.+,/, ""));
+    };
     reader.readAsDataURL(file);
   }
-  
 
   const GoogleLoginButton = (
     <GoogleLogin
-    clientId="930816262472-soqcqsgkaqf4t6e94hecl3il2ubgbhis.apps.googleusercontent.com"
-    buttonText="Login"
-    onSuccess={setAccessTokenState}
-    onFailure={responseGoogle}
-    cookiePolicy={'single_host_origin'}
-    scope='https://www.googleapis.com/auth/cloud-platform'
-  />
-  )
+      clientId="930816262472-soqcqsgkaqf4t6e94hecl3il2ubgbhis.apps.googleusercontent.com"
+      buttonText="Login"
+      onSuccess={setAccessTokenState}
+      onFailure={responseGoogle}
+      cookiePolicy={"single_host_origin"}
+      scope="https://www.googleapis.com/auth/cloud-platform"
+    />
+  );
 
-  return (
-    accessToken ? sendPhotoForm : GoogleLoginButton
-  )
+  return accessToken ? sendPhotoForm : GoogleLoginButton;
 }
